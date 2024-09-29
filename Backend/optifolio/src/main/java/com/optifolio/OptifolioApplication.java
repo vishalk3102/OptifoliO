cfc43ebfec2e86df670fa0039c8bbf76ae042c71
vishal <vishal.k3102@gmail.com>
2024-09-29 16:04:15 +0530

    folder setup--backend

---
+++ Backend/optifolio/src/main/java/com/optifolio/OptifolioApplication.java
@@ -0,0 +1,13 @@
+package com.optifolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptifolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptifolioApplication.class, args);
+	}
+
+}
---
+++ Backend/optifolio/src/test/java/com/optifolio/OptifolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.optifolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptifolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
